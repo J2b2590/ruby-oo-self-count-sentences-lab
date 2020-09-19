ba7bf8df0ab59fccaceb3bb815d6afc26fa723ae
J2b2590 <J2b2590@gmail.com>
2020-09-19 18:33:50 -0500

    finished lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
+    self.include?(".")
     
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
-  def count_sentences
 
+  def count_sentences
+   
+    sent = self.split(/[?!.]/)
+
+    count = 0
+
+    sent.each do |i|
+        if i.empty?
+          count = count
+          else
+            count += 1
+        end
+    end
+    count
   end
 end
